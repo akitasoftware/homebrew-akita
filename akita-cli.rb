17ee896f8fdec318805ccfd05a2458ea2255bfbe
Scott Maher <scott@akitasoftware.com>
2020-07-31 11:54:32 -0700

    Bump CLI to 0.3.4

--- akita-cli.rb
+++ akita-cli.rb
@@ -1,14 +1,14 @@
 class AkitaCli < Formula
   desc "The Akita CLI"
   homepage "https://www.akitasoftware.com"
-  version "0.3.3"
+  version "0.3.4"
   bottle :unneeded
   
   depends_on "libpcap"
 
   if OS.mac?
-    url "https://releases.akita.software/cli/0.3.3/akita_0.3.3_darwin_amd64.zip"
-    sha256 "1c34d44300cbed2bcc9c7c2014878c2e4083475e8c8e2d3a0039d8f107e72856" 
+    url "https://releases.akita.software/cli/0.3.4/akita_0.3.4_darwin_amd64.zip"
+    sha256 "781b99dda01819ab7d509d8b4bddff7b0ece8ed041b3e9e46f8dae0bbe2c2c35" 
   end
 
   def install
