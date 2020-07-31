144b646a47dfc772ac7308b1e0f0e53facf864e2
Scott Maher <scott@akitasoftware.com>
2020-07-31 11:32:13 -0700

    Bump akita-cli to 0.3.3.

--- akita-cli.rb
+++ akita-cli.rb
@@ -1,12 +1,12 @@
 class AkitaCli < Formula
   desc "The Akita CLI"
   homepage "https://www.akitasoftware.com"
-  version "0.3.2"
+  version "0.3.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://releases.akita.software/cli/0.3.2/akita_0.3.2_darwin_amd64.zip"
-    sha256 "a15f36e08781d17709d2817d472b5023841b75772696db5cb3a094a69eecf175" 
+    url "https://releases.akita.software/cli/0.3.3/akita_0.3.3_darwin_amd64.zip"
+    sha256 "1c34d44300cbed2bcc9c7c2014878c2e4083475e8c8e2d3a0039d8f107e72856" 
   end
 
   def install
@@ -14,3 +14,6 @@ class AkitaCli < Formula
   end
 
 end
+
+
+
