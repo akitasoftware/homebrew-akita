45f5fb54ffa446d5c33a41d85f19154379f789a9
Scott Maher <scott@akitasoftware.com>
2020-08-13 21:04:30 -0700

    Bump to 0.3.6. Remove libpcap dep.

--- akita-cli.rb
+++ akita-cli.rb
@@ -1,14 +1,12 @@
 class AkitaCli < Formula
   desc "The Akita CLI"
   homepage "https://www.akitasoftware.com"
-  version "0.3.5"
+  version "0.3.6"
   bottle :unneeded
-  
-  depends_on "libpcap"
 
   if OS.mac?
-    url "https://releases.akita.software/cli/0.3.5/akita_0.3.5_darwin_amd64.zip"
-    sha256 "7510cf78e7d11a1634ed28ad7b0d4d53d537c94bf864bfc5ce3538b820e17c76" 
+    url "https://releases.akita.software/cli/0.3.6/akita_0.3.6_darwin_amd64.zip"
+    sha256 "bd954a4a05ff76e92c3915b21331afc9594e829f6a95f63b4831dd9af5b95a4e"
   end
 
   def install
