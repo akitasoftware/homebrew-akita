11609804df5a25c33ac919e4f57cc8360a3db3ed
Scott Maher <scott@akitasoftware.com>
2020-09-11 15:40:04 -0700

    Update akita-cli to 0.4.4

--- akita-cli.rb
+++ akita-cli.rb
@@ -1,12 +1,12 @@
 class AkitaCli < Formula
   desc "The Akita CLI"
   homepage "https://www.akitasoftware.com"
-  version "0.4.3"
+  version "0.4.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://releases.akita.software/cli/0.4.3/akita_0.4.3_darwin_amd64.zip"
-    sha256 "0083ae66fc8ace32abab18e81faa79653c6b0ad48a4ff37d5b307b3058ea6ded"
+    url "https://releases.akita.software/cli/0.4.4/akita_0.4.4_darwin_amd64.zip"
+    sha256 "52bf0faf5ccc7d6f95c14a7aee2fdbdd1761870a2f857ca626460c6b488fd5f3"
   end
 
   def install
