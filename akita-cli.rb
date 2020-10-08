18a2902f47c5bc7d1c8626bbb2650901a105dcdc
Cole <63367934+thatplguy@users.noreply.github.com>
2020-10-08 11:08:27 -0700

    Upgrade CLI to version 0.5.0 (#3)

--- akita-cli.rb
+++ akita-cli.rb
@@ -1,12 +1,12 @@
 class AkitaCli < Formula
   desc "The Akita CLI"
   homepage "https://www.akitasoftware.com"
-  version "0.4.6"
+  version "0.5.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://releases.akita.software/cli/0.4.6/akita_0.4.6_darwin_amd64.zip"
-    sha256 "5c955fdf877e1ad6dc243566363d864709ef7302d6cbc1e371de5b08970b27d9"
+    url "https://releases.akita.software/cli/0.5.0/akita_0.5.0_darwin_amd64.zip"
+    sha256 "a1ec660cf3b1577ae1ef80fb99233b521e9c6ec406d743cbc293dccb1977653f"
   end
 
   def install
