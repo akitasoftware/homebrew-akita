5fa78bfb56dc0cdbddf0c35d12b12bb320f3d425
Cole <63367934+thatplguy@users.noreply.github.com>
2020-12-11 15:35:42 -0800

    Deploy 0.6.1 (#7)

--- akita-cli.rb
+++ akita-cli.rb
@@ -1,12 +1,12 @@
 class AkitaCli < Formula
   desc "The Akita CLI"
   homepage "https://www.akitasoftware.com"
-  version "0.6.0"
+  version "0.6.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://releases.akita.software/cli/0.6.0/akita_0.6.0_darwin_amd64.zip"
-    sha256 "14d4c3633fad8ec09228f52c3cab37c8d1738605eb1cd5a4c627d0b69750d3c9"
+    url "https://releases.akita.software/cli/0.6.1/akita_0.6.1_darwin_amd64.zip"
+    sha256 "11b4de9c9ff9ddeef1e768b7a423ea6d4dabeead1129dad26d856811d393769a"
   end
 
   def install
