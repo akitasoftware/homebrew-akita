0c55bd5a07785884c3d4881647f4181139dca571
Cole Schlesinger <cole@akitasoftware.com>
2021-04-29 14:42:10 -0700

    Release 0.13.0

--- akita-cli.rb
+++ akita-cli.rb
@@ -1,16 +1,16 @@
 class AkitaCli < Formula
   desc "The Akita CLI"
   homepage "https://www.akitasoftware.com"
-  version "0.12.0"
+  version "0.13.0"
   bottle :unneeded
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://releases.akita.software/cli/0.12.0/akita_0.12.0_darwin_arm64.zip"
-      sha256 "eaef4b4ba73b95b225106b03b5eea0c5ae09866db0487995aff69ca0ffcc4466"
+      url "https://releases.akita.software/cli/0.13.0/akita_0.13.0_darwin_arm64.zip"
+      sha256 "800ec26e6f6d8aeeb4e51f53f8bab6481124ce086f4d4571e4080e5d37c4670f"
     else
-      url "https://releases.akita.software/cli/0.12.0/akita_0.12.0_darwin_amd64.zip"
-      sha256 "83575c62174b9ba6dde93004bb1cc3d2671125b43714026caf3b4264dba65165"
+      url "https://releases.akita.software/cli/0.13.0/akita_0.13.0_darwin_amd64.zip"
+      sha256 "52f19d2d46f91bf6581bf71b88605dbeda2440a0bc95a20d7bf370fa4d8d2609"
     end
   end
 
