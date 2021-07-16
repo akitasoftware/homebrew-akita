e974993e12da049a4dd32917b2f96e09e324e894
Cole Schlesinger <cole@akitasoftware.com>
2021-07-16 12:03:35 -0700

    Release CLI v0.17.2 for AMD
    The ARM release is not yet available but will follow shortly.

--- akita-cli.rb
+++ akita-cli.rb
@@ -1,16 +1,17 @@
 class AkitaCli < Formula
   desc "The Akita CLI"
   homepage "https://www.akitasoftware.com"
-  version "0.17.1"
+  version "0.17.2"
   bottle :unneeded
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://releases.akita.software/cli/0.17.1/akita_0.17.1_darwin_arm64.zip"
-      sha256 "265d606def32fd392e86551a613dee0abb516bef4cb270547ef9b959215de6cb"
+      # XXX(cns): Remove temporarily until version 0.17.2 is available for ARM.
+      # url "https://releases.akita.software/cli/0.17.2/akita_0.17.2_darwin_arm64.zip"
+      # sha256 "265d606def32fd392e86551a613dee0abb516bef4cb270547ef9b959215de6cb"
     else
-      url "https://releases.akita.software/cli/0.17.1/akita_0.17.1_darwin_amd64.zip"
-      sha256 "f755001cb4fbb0accb296a04a8beb0fc4d614aed95eee177e26b86d246e711ce"
+      url "https://releases.akita.software/cli/0.17.2/akita_0.17.2_darwin_amd64.zip"
+      sha256 "70ef87277f81aedab3abca79bcb58800054c3fbe2fd9e33ed77ad9a2d587374d"
     end
   end
 
