3a4a97e1b70cb12dbe44f5d470a1300b7171a457
Jed Liu <liujed@users.noreply.github.com>
2021-11-11 11:53:25 -0800

    Release 0.19.1

--- akita-cli.rb
+++ akita-cli.rb
@@ -1,16 +1,16 @@
 class AkitaCli < Formula
   desc "The Akita CLI"
   homepage "https://www.akitasoftware.com"
-  version "0.19.0"
+  version "0.19.1"
   bottle :unneeded
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://releases.akita.software/cli/0.19.0/akita_0.19.0_darwin_arm64.zip"
-      sha256 "ed8625e6ec3761c942059f80c2e72ffac967984ae6119db0a8c275604d7539db"
+      url "https://releases.akita.software/cli/0.19.1/akita_0.19.1_darwin_arm64.zip"
+      sha256 "58cc1b55b3b4103004fd8f0fd71672622fca72bc24fa539c6fb5ad509d129309"
     else
-      url "https://releases.akita.software/cli/0.19.0/akita_0.19.0_darwin_amd64.zip"
-      sha256 "68b005f0f8ad2de7dff8a01304bdd6c885b88920e4b879af6d7ee831380b69d5"
+      url "https://releases.akita.software/cli/0.19.1/akita_0.19.1_darwin_amd64.zip"
+      sha256 "8e9e4f90c5734542241cf791f7c7be319227f259c5c4c485a06471ae81fbc42c"
     end
   end
 
