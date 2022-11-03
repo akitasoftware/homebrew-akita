f3ec27c808145fc9dee4ef658fd6ec47912dc7fa
Jed Liu <liujed@users.noreply.github.com>
2022-11-03 15:26:09 -0700

    Release 0.21.16

--- akita-cli.rb
+++ akita-cli.rb
@@ -1,15 +1,15 @@
 class AkitaCli < Formula
   desc "The Akita CLI"
   homepage "https://www.akitasoftware.com"
-  version "0.21.15"
+  version "0.21.16"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://releases.akita.software/cli/0.21.15/akita_0.21.15_darwin_arm64.zip"
-      sha256 "8dc24439cef5c3266d413d40e9b7138cea1833c6e4c5cd731aba587795eaf0ae"
+      url "https://releases.akita.software/cli/0.21.16/akita_0.21.16_darwin_arm64.zip"
+      sha256 "f413d6e3183a262f1c41e63de3a670d105685502f74b3c1770e416ca091fb3d0"
     else
-      url "https://releases.akita.software/cli/0.21.15/akita_0.21.15_darwin_amd64.zip"
-      sha256 "a65f9603d1ee7b37c14d6cc66196465576dcbe62a9e659cb3bfaed67b370a31b"
+      url "https://releases.akita.software/cli/0.21.16/akita_0.21.16_darwin_amd64.zip"
+      sha256 "a1e28161bb90cd6e871f60b15587bbff0b7f26c2d11ad39c64857b1c1d3ee7aa"
     end
   end
 
